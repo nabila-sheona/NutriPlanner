84c63b4ee64575155514e7fa924d7adbb750bbba
Namisa Najah <namisa.najah.raisa@gmail.com>
2025-08-09 10:03:38 +0600

    feat : added tailwind

---
+++ client/tailwind.config.js
@@ -0,0 +1,11 @@
+    // tailwind.config.js
+    module.exports = {
+      content: [
+        "./src/**/*.{html,js,jsx,ts,tsx}", // Example path for a React project
+        // Add other paths as needed for your project structure
+      ],
+      theme: {
+        extend: {},
+      },
+      plugins: [],
+    }
