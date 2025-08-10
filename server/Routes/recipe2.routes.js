d0a0c5b7b80db277fb2c12e12783b8c705312415
nstuli <nusratsiddiquetuli@gmail.com>
2025-08-10 13:08:04 +0600

    route : recipe 2 history route update

--- server/Routes/recipe2.routes.js
+++ server/Routes/recipe2.routes.js
@@ -1,11 +1,25 @@
-// server/Routes/recipe.routes2.js
 const express = require('express');
-const { likeRecipe, getLikedRecipes } = require('../Controller/recipe.controller2.js');
-const auth = require('../middleware/auth.js');
-
 const router = express.Router();
+const auth = require('../middleware/auth');
 
-router.post('/like/:recipeId', auth, likeRecipe);
-router.get('/liked', auth, getLikedRecipes);
+// Explicitly import all controller functions
+const recipeController = require('../Controller/recipe.controller2');
+
+console.log('Initializing recipe routes...');
+
+// History route with debug middleware
+router.get('/history', 
+  (req, res, next) => {
+    console.log('\n[DEBUG] /history route hit');
+    console.log('Request headers:', req.headers);
+    next();
+  },
+  auth,
+  recipeController.getRecipeHistory
+);
+
+// Other routes
+router.post('/like/:recipeId', auth, recipeController.likeRecipe);
+router.get('/liked', auth, recipeController.getLikedRecipes);
 
 module.exports = router;
