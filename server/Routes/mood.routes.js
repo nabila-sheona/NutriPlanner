72143ece1949ea2acbc76fa77ffe72da86f06be2
Namisa Najah <namisa.najah.raisa@gmail.com>
2025-08-16 10:32:44 +0600

    cleanup

--- server/Routes/mood.routes.js
+++ server/Routes/mood.routes.js
@@ -1,18 +1,21 @@
 // server/Routes/mood.routes.js
-const express = require('express');
-const { logMood, getMoodHistory, generateMoodRecipes } = require('../Controller/mood.controller.js');
-const auth = require('../middleware/auth');
-const { getMoodGraphData } = require("../Controller/mood.controller");
-
+const express = require("express");
+const {
+  logMood,
+  getMoodHistory,
+  generateMoodRecipes,
+  getMoodGraphData,
+} = require("../Controller/mood.controller.js");
+const auth = require("../middleware/auth");
 
 const router = express.Router();
 
-router.post('/moods', auth, logMood);
-router.get('/history', auth, getMoodHistory);
-router.post('/recipes/generate', auth, generateMoodRecipes);
+router.post("/moods", auth, logMood);
+router.get("/history", auth, getMoodHistory);
+router.post("/recipes/generate", auth, generateMoodRecipes);
 
-router.get('/moods/graph', auth, (req, res, next) => {
-  console.log('[MoodRoutes] /moods/graph route called');
+router.get("/moods/graph", auth, (req, res, next) => {
+  console.log("[MoodRoutes] /moods/graph route called");
   getMoodGraphData(req, res, next);
 });
 
